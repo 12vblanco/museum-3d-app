3575c2d977986ae30e9bf496f67fcc497fc9c715
Victor Blanco <12vblanco@gmail.com>
2026-04-29 14:34:42 +0800

    loading spinner added2

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,11 +1,14 @@
+import { Html } from '@react-three/drei';
 import './LoadingSpinner.css';
 
 function LoadingSpinner() {
   return (
-    <div className="loading-container">
-      <div className="blob"></div>
-      <p className="loading-text">Loading</p>
-    </div>
+    <Html center>
+      <div className="loading-container">
+        <div className="blob"></div>
+        <p className="loading-text">Loading</p>
+      </div>
+    </Html>
   );
 }
 
