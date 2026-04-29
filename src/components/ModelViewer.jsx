cb1976e80be4ee17875510eb7fe20b09a3e69372
Victor Blanco <12vblanco@gmail.com>
2026-04-29 13:56:48 +0800

    size of the model adjusted3

--- src/components/ModelViewer.jsx
+++ src/components/ModelViewer.jsx
@@ -15,12 +15,12 @@ function ModelViewer({ modelPath }) {
       const size = box.getSize(new THREE.Vector3());
       
       // Make table larger: target height 1.4 units (was 1.2)
-      const targetHeight = 1.8;
+      const targetHeight = 1;
       const scale = targetHeight / size.y;
       
       modelRef.current.scale.setScalar(scale);
       // Position so bottom of model sits on "floor" (y=0)
-      const bottomY = center.y - size.y/2;
+      const bottomY = center.y - size.y/1;
       modelRef.current.position.set(
         -center.x * scale,
         -bottomY * scale,
